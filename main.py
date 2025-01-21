fda20d1a8e0c81e9b83d5551da787e979a0ede09
akhivykUA <artsiom.khivyk@underarmour.com>
2025-01-21 18:26:56 +0100

    upd site

--- main.py
+++ main.py
@@ -7,38 +7,40 @@ bot = Bot('7878345097:AAGArCEZCN2Q1_DVYdKtEM-XwMadYgps9bI')
 dp = Dispatcher()
 router = Router()
 
+
 @router.message(F.text == '/start')
 async def start(message: Message):
     user_id = message.from_user.id
     user_name = message.from_user.full_name
-    user_photo = message.from_user.photo
 
-    if user_photo:
-        photo_url = await bot.get_user_profile_photos(user_id)
-        if photo_url.photos:
-            user_photo_url = photo_url.photos[0][-1].file_id
-            user_photo_url = await bot.get_file(user_photo_url)
-            photo_url = f"https://api.telegram.org/file/bot{bot.token}/{user_photo_url.file_path}"
-        else:
-            photo_url = "https://cdn-icons-png.flaticon.com/512/847/847969.png"
-    else:
-        photo_url = "https://cdn-icons-png.flaticon.com/512/847/847969.png"
+    # Получение фото пользователя
+    photo_url = "https://cdn-icons-png.flaticon.com/512/847/847969.png"  # Фото по умолчанию
+    photos = await bot.get_user_profile_photos(user_id)
 
+    if photos.photos:
+        # Получаем URL фото
+        file_id = photos.photos[0][-1].file_id
+        file = await bot.get_file(file_id)
+        photo_url = f"https://api.telegram.org/file/bot{bot.token}/{file.file_path}"
+
+    # Добавление параметра версии в URL
     version_param = f"?v={int(time.time())}"
     url = f'https://wwersder.github.io/alitabot/index.html{version_param}&photo={photo_url}'
 
+    # Создание кнопки с веб-приложением
     markup = InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text='GAME', web_app=WebAppInfo(url=url))]
-        ],
-        resize_keyboard=True
+        ]
     )
 
     await message.answer('Ссылка на DEV приложение алиты 👇', reply_markup=markup)
 
+
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
